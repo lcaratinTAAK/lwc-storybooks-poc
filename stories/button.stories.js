8f0d3c6e5b4054d35afcf2a42f988ea02c98a10b
lcaratin <leo.caratin@taak.com.br>
2025-04-26 22:19:50 -0300

    adding lightning-button

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -1,35 +1,13 @@
 import { storiesOf } from '@storybook/html';
 import buildAndRegisterCustomElement from './utils/build-custom-element';
-import Button from 'c/button';
 
-buildAndRegisterCustomElement('c-button', Button);
+import Button from 'lightning/button';
+
+buildAndRegisterCustomElement('lightning-button', Button);
 
 storiesOf('Button', module)
-    .add('base', () => `
-        <div class="slds-p-around_medium">
-            <c-button label="Base Button"></c-button>
-        </div>    
-    `)
-    .add('neutral', () => `
-        <div class="slds-p-around_medium">
-            <c-button variant="neutral" label="Neutral Button"></c-button>
-        </div>    
-    `)
-    .add('brand', () => `
-        <div class="slds-p-around_medium">
-            <c-button variant="brand" label="Brand Button"></c-button>
-        </div>    
-    `)
-    .add('destructive', () => `
-        <div class="slds-p-around_medium">
-            <c-button variant="destructive" label="Destructive Button"></c-button>
-        </div>    
-    `)
-    .add('success', () => `
-        <div class="slds-p-around_medium">
-            <c-button variant="success" label="Success Button"></c-button>
-        </div>    
-    `);
-
-
-   
+  .add('base', () => `
+    <div class="slds-p-around_medium">
+      <lightning-button label="Base Button"></lightning-button>
+    </div>
+  `);
